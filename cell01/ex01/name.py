49f5455798f3ed052e4797a29c601bdaba3eb363
nphootee <Narisara171166@gmail.com>
2025-06-17 04:48:32 +0000

    fdi

---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Narissara"
+last_name = "Phooteeka"
+whole_name = first_name + " "+ last_name
+print(whole_name)
--- cell01/ex02/age.py
+++
@@ -1,4 +0,0 @@
-ori = 18
-add = 42
-my_age = ori + add
-print (my_age)
