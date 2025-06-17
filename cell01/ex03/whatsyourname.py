c0bc733bc0e05492717cc3f83d477cb4f572e3a3
nphootee <Narisara171166@gmail.com>
2025-06-17 05:03:20 +0000

    jhty

--- cell01/ex03/whatsyourname.py
+++ cell01/ex03/whatsyourname.py
@@ -1,4 +1,4 @@
 first_name = input ("Hey, what's your first name? : Narissara")
-last_name + input("And your last name? : Phooteeka")
+last_name = input("And your last name? : Phooteeka")
 print("well,pleased to meet you,", first_name, last_name)
 
